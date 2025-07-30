87beba862b3e65d88727a0d8fae9801d886e90e5
Saqib Asif <devopsaqib@gmail.com>
2025-07-30 14:21:13 +0500

    Update server.js port

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const fetch = (...args) =>
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; // ✅ Zeabur fix
 
 app.use(cors());
 app.use(express.json());
